61746342f4f140c8b1c4bd6cb00e5c9ffe530ba9
Enzo <81955852+Enzooo22@users.noreply.github.com>
2026-09-19 12:47:18 +0800

    Add files via upload

---
+++ WebApplication1/Dolotallas_firstprogram/Program.cs
@@ -0,0 +1,48 @@
+﻿
+//This Hello World 
+Console.WriteLine("Hello, World!");
+
+//5 Things about me
+Console.WriteLine("I like watching anime and movies");
+Console.WriteLine("I'm 19 Years Old");
+Console.WriteLine("I like to explore new things");
+Console.WriteLine("I also like playing video games");
+Console.WriteLine("I actually care in my academics");
+
+// Variable Declaration
+string name = "Christian";
+int age = 19;
+
+Console.WriteLine (name);
+Console.WriteLine (age);
+Console.WriteLine ($"My name is {name}, my age is {age}!");
+
+// Operation Symbols
+int num1 = 100;
+int num2 = 5;
+
+int add = num1 + num2;
+int min = num1 - num2;
+int mul = num1 * num2;
+int div = num1 / num2;
+int mol = num1 % num2;
+
+Console.WriteLine (add +  ", " + min + ", " + mul + ", " + div + ", " + mol);
+
+// Input User 
+Console.WriteLine("Enter your Name: ");
+String nig = Console.ReadLine();
+
+
+Console.WriteLine("Enter your age: ");
+int ger = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Zip Code");
+int ass = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Hi, my name is " + nig);
+Console.WriteLine($"My age is {ger}");
+Console.WriteLine($"My Zip Code is {ass}");
+
+
+
---
+++ WebApplication1/Esteban_firstprogram/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Hello, World!");
+
+Console.WriteLine("I love to learn new things");
+Console.WriteLine("I'm 19 Years Old");
+Console.WriteLine("I'm generous");
+Console.WriteLine("I love exploring the gaming culture");
+Console.WriteLine("I hate Christian Dolotallas");
+
+String name = "Enzo";
+int age = 19;
+
+Console.WriteLine(name);
+Console.WriteLine(age);
+Console.WriteLine($"Ako si {name} and my age is {age}");
+
+int num1 = 100000;
+int num2 = 77013;
+ 
+Console.WriteLine(num1 + num2);
+Console.WriteLine(num1 - num2);
+Console.WriteLine(num1 * num2);
+Console.WriteLine(num1 / num2);
+Console.WriteLine(num1 % num2);
+
+Console.WriteLine("Enter your name");
+String pangalan = Console.ReadLine();
+Console.WriteLine("Hi, my name is " + pangalan);
+
+Console.WriteLine("Enter your age");
+int edad = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine($"My age is {edad}");
+
+Console.WriteLine("Lagay mo edad mo ulit");
+int age2 = int.Parse(Console.ReadLine());
+Console.WriteLine($"My edad ulit is {age2}");
